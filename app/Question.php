6f1094cbe775292e4bf5c896b5fe7f848ae634e7
Mark Briones <wp.markbriones@gmail.com>
2020-01-06 17:49:57 +0800

    populate users & questions tables and seed

--- app/Question.php
+++ app/Question.php
@@ -13,7 +13,7 @@ class Question extends Model
     
     public function user()
     {
-        return $this->belongTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function setTitleAttribute($value)
