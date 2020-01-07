ead6007cfc259a65e8f3ece2ac441f2b4e6c7a68
Mark Briones <wp.markbriones@gmail.com>
2020-01-07 10:42:17 +0800

    Display all questions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
         factory(App\User::class, 3)->create()->each(function($u){
             $u->questions()->saveMany(
-                factory(App\Question::class, rand(1, 10))->make()
+                factory(App\Question::class, rand(1, 10))->make() // make() create objects and store in memory
             );
         });
     }
