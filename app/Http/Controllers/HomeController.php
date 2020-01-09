25a58e3cfa4f28418752caf3fdc52b57ee3f0d70
Mark Briones <wp.markbriones@gmail.com>
2020-01-09 10:24:57 +0800

    Added vote controls on question and answers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Question;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $questions = Question::with('user')->latest()->paginate(5);
+        return view('questions.index', compact('questions'));
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -49613,8 +49613,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /mnt/c/Users/mdb/Documents/LaraVue/1/laravel-qa/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /mnt/c/Users/mdb/Documents/LaraVue/1/laravel-qa/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\MDB\Documents\LaraVue\1\laravel-qa\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\MDB\Documents\LaraVue\1\laravel-qa\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
