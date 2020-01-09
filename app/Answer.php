580fc5aa52e055093cdfb95fc4e14d442f07121a
Mark Briones <wp.markbriones@gmail.com>
2020-01-09 17:59:29 +0800

    update best_answer_id on questions table when an answer removed using eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -35,12 +35,24 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer) {
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+
+            if ($question->best_answer_id == $answer->id) {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
+
         });
     }
 
     public function getCreatedDateAttribute() // called in index.blade.php as $question->created_date
     {
         return $this->created_at->diffForHumans(); // diffForHumans() method returns => ex. 1 day ago 
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -17,7 +17,7 @@
                             <a title="This answer is not useful" class="vote-down off mb-2">
                                 <i class="fa fa-caret-down fa-2x"></i>
                             </a>
-                            <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                            <a title="Mark this answer as best answer" class="{{ $answer->status }} mt-2">
                                 <i class="fa fa-check fa-lg"></i>
                             </a>
                         </div>
