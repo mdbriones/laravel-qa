d97572b769224bd84640895a5f2e1a44d4e95524
Mark Briones <wp.markbriones@gmail.com>
2020-01-15 17:12:54 +0800

    Make the vote answer working

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -10,13 +10,25 @@
                 @foreach ($answers as $answer)
                     <div class="media">
                         <div class="d-flex flex-column vote-controls">
-                            <a title="This answer is useful" class="vote-up">
+                            <a title="This answer is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
+                                onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{ $answer->id }}').submit();">
                                 <i class="fa fa-caret-up fa-2x"></i>
-                            </a>
-                            <span class="votes-count">211</span>
-                            <a title="This answer is not useful" class="vote-down off mb-2">
+                            </a> 
+                            <form style="display: none;" action="/answers/{{ $answer->id }}/vote" id="up-vote-answer-{{ $answer->id }}" method="post">
+                                @csrf
+                                <input type="hidden" name="vote" value="1">
+                            </form>
+
+                            <span class="votes-count">{{ $answer->vote_count }}</span>
+
+                            <a title="This answer is not useful" class="vote-down {{ Auth::guest() ? 'off' : '' }} mb-2"
+                                onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{ $answer->id }}').submit();">
                                 <i class="fa fa-caret-down fa-2x"></i>
                             </a>
+                            <form style="display: none;" action="/answers/{{ $answer->id }}/vote" id="down-vote-answer-{{ $answer->id }}" method="post">
+                                @csrf
+                                <input type="hidden" name="vote" value="-1">
+                            </form>
 
                             @can('accept', $answer)
                                 <a title="Mark this answer as best answer"
